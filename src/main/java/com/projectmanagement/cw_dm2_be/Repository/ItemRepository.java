c036ef654ae1ee60eb9a6d9d9e316dd387819e43
Your Name <thashila10@gmail.com>
2025-04-30 01:32:44 +0530

    item controller update

--- src/main/java/com/projectmanagement/cw_dm2_be/Repository/ItemRepository.java
+++ src/main/java/com/projectmanagement/cw_dm2_be/Repository/ItemRepository.java
@@ -6,7 +6,13 @@ import com.projectmanagement.cw_dm2_be.Model.Item;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
+import java.util.List;
+
 
 public interface ItemRepository extends MongoRepository<Item, Integer> {
     Optional<Item> findById(int Item_id);
+
+    // Find products by category
+    List<Item> findByCategory(String Item_Category);
+
 }
