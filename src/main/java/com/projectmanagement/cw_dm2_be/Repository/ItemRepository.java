c593e73bdc415a7aacfdbf21bdeef3e06434edc8
kalu4136b <kalharalakshitha25@gmail.com>
2025-04-30 08:42:12 +0530

    dsa
    ads

--- src/main/java/com/projectmanagement/cw_dm2_be/Repository/ItemRepository.java
+++ src/main/java/com/projectmanagement/cw_dm2_be/Repository/ItemRepository.java
@@ -5,8 +5,10 @@ import com.projectmanagement.cw_dm2_be.Model.Item;
 //import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends MongoRepository<Item, Integer> {
     Optional<Item> findById(int Item_id);
+    List<Item> findByCategory(String category);
 }
